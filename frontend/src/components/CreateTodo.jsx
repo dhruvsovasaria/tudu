ca6d3c5fb722717ea04162a58833688e58acefd8
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2024-06-03 16:12:50 +0530

    init fe

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,1 +1,9 @@
-sfc;
+export default function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="desc" />
+      <button>add todo</button>
+    </div>
+  );
+}
